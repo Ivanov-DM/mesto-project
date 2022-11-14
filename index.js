3b17a2305e35cbfcd799764f4345673c199b8a73
Dmitry Ivanov <di.ivanov2018@gmail.com>
2022-11-14 18:24:49 +0300

    feat: add render initial cards

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileUserName = document.querySelector('.profile__user-name');
 const profileUserAbout = document.querySelector('.profile__user-about');
@@ -8,6 +35,9 @@ const popupCloseButton = document.querySelector('.popup__close-button');
 const editProfileForm = document.forms['edit-profile-form'];
 const userNameFormField = editProfileForm.elements.userName;
 const userAboutFormField = editProfileForm.elements.userAbout;
+
+const cards = document.querySelector('.cards');
+const cardTemplate = document.querySelector('#card-template').content;
 
 function openPopup() {
     userNameFormField.value = profileUserName.textContent;
@@ -26,8 +56,27 @@ function editProfileFormSubmitHandler(event) {
     closePopup();
 }
 
+function createCard(src, title) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    cardImage.src = src;
+    card.alt = title;
+    cardTitle.textContent = title;
+    return card
+}
+
+function renderInitialCards() {
+    for (let i = 0; i < initialCards.length; i++) {
+        cards.append(createCard(initialCards[i].link, initialCards[i].name));
+    }
+}
+
 profileEditButton.addEventListener('click', openPopup);
 
 popupCloseButton.addEventListener('click', closePopup);
 
 editProfileForm.addEventListener('submit', editProfileFormSubmitHandler);
+
+renderInitialCards();
+

71b0dac94173cb268f3dd110c07db2c37262af2b
Dmitry Ivanov <di.ivanov2018@gmail.com>
2022-11-17 15:12:23 +0300

    add ability to delete cards

--- index.js
+++ index.js
@@ -56,13 +56,20 @@ function createCard(src, title) {
     const cardImage = card.querySelector('.card__image');
     const cardTitle = card.querySelector('.card__title');
     const cardLikeButton = card.querySelector('.card__like-button');
+    const cardDeleteButton = card.querySelector('.card__delete-button');
 
     cardImage.src = src;
     cardImage.alt = title;
     cardTitle.textContent = title;
+
     cardLikeButton.addEventListener('click', function(event) {
         event.target.classList.toggle('card__like-button_active');
-    })
+    });
+
+    cardDeleteButton.addEventListener('click', function (event) {
+        event.target.closest('.card').remove();
+    });
+
     return card
 }
 

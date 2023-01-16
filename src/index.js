98169878e1edd26d18a5ad658570b9c1c2f40ae8
Dmitry Ivanov <di.ivanov2018@gmail.com>
2023-01-16 23:40:09 +0300

    feat: add validation for popup elements

--- src/index.js
+++ src/index.js
@@ -21,6 +21,73 @@ const placeLinkFormField = addCardForm.elements.placeLink;
 const cards = document.querySelector('.cards');
 const cardTemplate = document.querySelector('#card-template').content;
 
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('form__input-field_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('form__input-error_active');
+}
+
+const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('form__input-field_type_error');
+    errorElement.classList.remove('form__input-error_active');
+    errorElement.textContent = '';
+}
+
+const isValid = (formElement, inputElement) => {
+    if (inputElement.validity.patternMismatch) {
+        inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+    } else {
+        inputElement.setCustomValidity('');
+    }
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+        hideInputError(formElement, inputElement);
+    }
+}
+
+const hasInvalidInput = inputList => {
+    return inputList.some(inputElement => {
+        return !inputElement.validity.valid
+    })
+}
+
+const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.disabled = true;
+        buttonElement.classList.add('form__submit-button_inactive');
+    } else {
+        buttonElement.disabled = false;
+        buttonElement.classList.remove('form__submit-button_inactive');
+    }
+}
+
+const setEventListeners = formElement => {
+    const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+    const buttonElement = formElement.querySelector('.form__submit-button');
+
+    toggleButtonState(inputList, buttonElement);
+
+    inputList.forEach(inputElement => {
+        inputElement.addEventListener('input', () => {
+            isValid(formElement, inputElement);
+            toggleButtonState(inputList, buttonElement);
+        });
+    });
+}
+
+const enableValidation = () => {
+    const formList = document.querySelectorAll('.form');
+    formList.forEach(formElement => {
+        formElement.addEventListener('submit', evt => {
+            evt.preventDefault();
+        });
+        setEventListeners(formElement);
+    });
+}
+
 function openPopup(popupEl) {
     popupEl.classList.add('popup_opened');
 }
@@ -106,9 +173,15 @@ popups.forEach(popupEl => popupEl.addEventListener('mousedown', function (evt) {
             placeTitleFormField.value = '';
             placeLinkFormField.value = '';
         }
+        const formElement = popupEl.querySelector('.form');
+        if (formElement) {
+            const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+            inputList.forEach(inputElement => hideInputError(formElement, inputElement));
+        }
         closePopup(popupEl);
     }
 }));
 
+enableValidation();
 renderInitialCards();
 
